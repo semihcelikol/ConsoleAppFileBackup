b168d73fe29e4ae6d694b3d7a59c1dfa76cb3434
semihcelikol <semihcelikol@outlook.com>
2017-09-29 11:26:45 +0300

    Kodda düzenlemeler yapıldı. Gereksiz kısımlar kaldırıldı.

--- ConsoleAppFileBackup/Program.cs
+++ ConsoleAppFileBackup/Program.cs
@@ -18,10 +18,10 @@ namespace ConsoleAppFileBackup
             string backupFile = string.Empty;
             string outputFile = string.Empty;
 
-            //yedek alınacak dosya yolu belirtilir..Örneğin c:\test
-            backupFile = @"Backup File Path";
-            //yedeğin nereye alınacağı belirtilir. Örneğin c:\testOutput
-            outputFile = @"OutPut File Path";
+            //Yedek alınacak dosya yolu belirtilir.Örneğin c:\Users\UserName\test
+            backupFile = @"C:\Users\UserName\backupFileName";
+            //Yedeğin nereye alınacağı belirtilir. Örneğin c:\Users\UserName\testOutput
+            outputFile = @"C:\Users\Semih\OutPutfileName\";
 
             //zipleme yapılır.
             program.zipFile(backupFile, outputFile);
@@ -34,36 +34,37 @@ namespace ConsoleAppFileBackup
         /// <param name="_outputFilePath"></param>
         public void zipFile(string _filePath, string _outputFilePath)
         {
-            string day, month, year;
+            string day, month, year, hour, minute, second;
 
             day = DateTime.UtcNow.Day.ToString();
             month = DateTime.UtcNow.Month.ToString();
             year = DateTime.UtcNow.Year.ToString();
+            hour = DateTime.Now.Hour.ToString();
+            minute = DateTime.UtcNow.Minute.ToString();
+            second = DateTime.Now.Second.ToString();
 
             try
             {
-                if (_outputFilePath != "")
-                {
-                    globalCount = 0;
-
-                    //Klasör altında ki dosyaları listeler
-                    DirectoryInfo dirInfoFile = new DirectoryInfo(_outputFilePath);
-                    FileInfo[] files = dirInfoFile.GetFiles();
-
-                    foreach (FileInfo fi in files)
-                    {
-                        globalCount++;
-                    }
-                }
-
                 //Zipleme işlemi başlatılır.
                 ZipFile zip = new ZipFile();
                 zip.AddItem(_filePath);
                 globalCount += 1;
-                FileStream fs = File.Create(_outputFilePath + "\\" + day + "-" + month + "-" + year + "- BackupFile-" + globalCount + ".zip");
+                FileStream fs = File.Create(_outputFilePath + "\\"
+                                           + day + "-"
+                                           + month + "-"
+                                           + year + "-"
+                                           + hour + "-"
+                                           + minute + "-"
+                                           + second +"- BackupFile.zip");
                 fs.Close();
 
-                zip.Save(_outputFilePath + "\\" + day +"-" + month + "-"+ year + "- BackupFile-" + globalCount + ".zip");
+                zip.Save(_outputFilePath + "\\"
+                        + day + "-"
+                        + month + "-"
+                        + year + "-"
+                        + hour + "-"
+                        + minute + "-"
+                        + second + "- BackupFile.zip");
 
                 Console.Write("Sıkıştırma işlemi başarılı.");
             }
